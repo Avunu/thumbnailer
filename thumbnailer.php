9828c3618fb25d5cde05f9e4a36deab50d737990
Kevin Shenk <kevin@avu.nu>
2025-03-11 15:40:56 -0400

    feat: allow wordpress pages

--- thumbnailer.php
+++ thumbnailer.php
@@ -1,19 +1,22 @@
 <?php
+
 /**
  * Plugin Name: Thumbnailer
  * Description: Thumbnailer integration for WordPress
  * Version: 1.0.0
- * Author: Thumbnailer
+ * Author: Avunu LLC
  * Text Domain: thumbnailer
  */
 
 // Exit if accessed directly
 if (!defined('ABSPATH')) exit;
 
-class Thumbnailer {
+class Thumbnailer
+{
     private $options;
 
-    public function __construct() {
+    public function __construct()
+    {
         // Define constants
         define('THUMBNAILER_VERSION', '1.0.0');
         define('THUMBNAILER_PLUGIN_DIR', plugin_dir_path(__FILE__));
@@ -23,7 +26,8 @@ class Thumbnailer {
         add_action('plugins_loaded', array($this, 'init'));
     }
 
-    public function init() {
+    public function init()
+    {
         // Load plugin options
         $this->options = get_option('thumbnailer_options', array(
             'post_ids' => '',
@@ -39,7 +43,8 @@ class Thumbnailer {
         add_action('wp_enqueue_scripts', array($this, 'enqueue_scripts'));
     }
 
-    public function add_admin_menu() {
+    public function add_admin_menu()
+    {
         add_options_page(
             'Thumbnailer Settings',
             'Thumbnailer',
@@ -49,7 +54,8 @@ class Thumbnailer {
         );
     }
 
-    public function register_settings() {
+    public function register_settings()
+    {
         register_setting(
             'thumbnailer_settings',
             'thumbnailer_options',
@@ -72,62 +78,67 @@ class Thumbnailer {
         );
     }
 
-    public function settings_section_callback() {
+    public function settings_section_callback()
+    {
         echo '<p>Configure which posts should use the thumbnailer functionality.</p>';
     }
 
-    public function post_ids_field_callback() {
+    public function post_ids_field_callback()
+    {
         $post_ids = isset($this->options['post_ids']) ? $this->options['post_ids'] : '';
         echo '<input type="text" id="post_ids" name="thumbnailer_options[post_ids]" value="' . esc_attr($post_ids) . '" class="regular-text" />';
         echo '<p class="description">Enter comma-separated post IDs where the thumbnailer should be active.</p>';
     }
 
-    public function sanitize_options($input) {
+    public function sanitize_options($input)
+    {
         $sanitized_input = array();
-        
+
         if (isset($input['post_ids'])) {
             // Remove spaces and validate that we only have numbers and commas
             $sanitized_input['post_ids'] = preg_replace('/[^0-9,]/', '', $input['post_ids']);
             // Remove any trailing commas
             $sanitized_input['post_ids'] = rtrim($sanitized_input['post_ids'], ',');
         }
-        
+
         return $sanitized_input;
     }
 
-    public function display_settings_page() {
-        ?>
+    public function display_settings_page()
+    {
+    ?>
         <div class="wrap">
             <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
             <form action="options.php" method="post">
-            <?php
+                <?php
                 settings_fields('thumbnailer_settings');
                 do_settings_sections('thumbnailer');
                 submit_button();
-            ?>
+                ?>
             </form>
         </div>
-        <?php
+    <?php
     }
 
-    public function enqueue_scripts() {
+    public function enqueue_scripts()
+    {
         // Check if we're on a single post page
-        if (is_single()) {
+        if (is_single() || is_page()) {
             global $post;
-            
+
             // Get the list of post IDs where thumbnailer should be active
             $post_ids = array();
             if (!empty($this->options['post_ids'])) {
                 $post_ids = array_map('intval', explode(',', $this->options['post_ids']));
             }
-            
+
             // Check if current post is in the list
             if (in_array($post->ID, $post_ids)) {
                 // Enqueue the thumbnailer script
                 wp_enqueue_script(
-                    'thumbnailer', 
+                    'thumbnailer',
                     THUMBNAILER_PLUGIN_URL . 'dist/thumbnailer.js',
-                    array(), 
+                    array(),
                     THUMBNAILER_VERSION,
                     true
                 );
